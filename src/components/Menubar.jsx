99ac846f29b0111337390284fc43a88d962d07f2
nyxlost <phumiphatnyxlost@gmail.com>
2024-06-15 00:14:54 +0700

    update menubar

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -7,31 +7,19 @@ function Menubar() {
       <nav className="w-full fixed top-0 bg-white">
         <div className="container mx-auto py-5 flex item-center justify-center">
           <ul className="hidden md:flex space-x-10 text-gray-600 font-sans text-xl uppercase">
-            <li>
-              <Link className="hover:text-gray-500" to="/advancesearch">
-                Advance Search
-              </Link>
-            </li>
+           
             <li>
               <Link className="hover:text-gray-500" to="/mastermind">
                 Master Mind
               </Link>
             </li>
-            <li>
-              <Link className="hover:text-gray-500" to="/detailcar">
-                Detail Car
-              </Link>
-            </li>
+            
             <li>
               <Link className="hover:text-gray-500" to="/decimalfinder">
                 Decimal Finder
               </Link>
             </li>
-            <li>
-              <Link className="hover:text-gray-500" to="/answer">
-                Answer 5&6
-              </Link>
-            </li>
+           
           </ul>
         </div>
       </nav>
